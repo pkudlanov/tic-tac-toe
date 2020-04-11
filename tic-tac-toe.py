537a253369eca373fe382cec36eabc600c988750
Pavel Kudlanov <pkudlanov@yahoo.com>
2020-04-10 21:45:56 -0400

    first to go, space available, board full check, replay or not, functions, and the start of the game place

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,19 +1,25 @@
-# board = ['¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹'] # Original Board BAK
+import random
+
+game_in_session = True
+players_turn = ''
 board = ['¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹']
+
 
 def player_marker_input():
     marker = ''
 
     while marker != 'X' and marker != 'O':
-        marker = input('Player 1, Choose X or O: ')
+        marker = input('Player 1, Choose X or O: ').upper()
 
     player1 = marker
 
     if player1 == 'X':
         player2 = 'O'
-    else: player2 = 'X'
+    else:
+        player2 = 'X'
 
     return (player1, player2)
+
 
 def display_board(board):
     print(f' {board[0]} | {board[1]} | {board[2]} ')
@@ -22,8 +28,10 @@ def display_board(board):
     print(f'---|---|---')
     print(f' {board[6]} | {board[7]} | {board[8]} ')
 
+
 def place_marker(board, marker, position):
     board[position - 1] = marker.upper()
+
 
 def win_check(baord, marker):
     win_conditions = [
@@ -37,37 +45,65 @@ def win_check(baord, marker):
         for pos in positions_set:
             if(board[pos] == marker):
                 win_or_not.append(marker)
-        
         if(len(win_or_not) == 3):
             return True
-        else: win_or_not = []
-    
+        else:
+            win_or_not = []
     return False
 
 
-player1_marker, player2_marker = player_marker_input()
+def first_to_go():
+    players = ['player1', 'player2']
 
-print(player1_marker, player2_marker)
+    player_to_go = players[random.randint(0, 1)]
+    print('r '.join(player_to_go.capitalize().split('r')) + ' will go first.')
+    return player_to_go
 
-display_board(board)
+
+def space_available(board, position):
+    return board[position] != 'X' and board[position] != 'O'
 
 
-## Tests
+def board_full_check(baord):
+    for char in board:
+        if (char != 'X' and char != 'O'):
+            return False
+    return True
 
-# place_marker(board, player1_marker, 3)
 
-# display_board(board)
+def replay_or_not():
+    answer = ''
 
-# hello = win_check(board, 'X')
-# print('RESULT OF FUNCTION', hello)
+    while answer != 'y' and answer != 'n':
+        answer = input("Would you like to play again? Press 'Y' or 'N'. >> ").lower()
+    if answer == 'y':
+        board = ['¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹']
+        display_board(board)
+        return True
+    else:
+        return False
 
-# Small Position Numbers for Reference
-# ¹
-# ²
-# ³
-# ⁴
-# ⁵
-# ⁶
-# ⁷
-# ⁸
-# ⁹
+
+def position_input(player):
+    position = 0
+    while not 1 <= position < 10:
+        position = int(input(player + ' where will you go? >> '))
+
+    return position
+
+
+print('Welcome to the wonderful classic of Tic Tac Toe!!!!')
+
+while game_in_session:
+    player1_marker, player2_marker = player_marker_input()
+
+    players_turn = first_to_go()
+
+    print(player1_marker, player2_marker)
+
+    display_board(board)
+    while not board_full_check(board):
+        if players_turn == 'player1':
+            place_marker(board, player1_marker, position_input('Player 1'))
+        elif players_turn == 'player2':
+            place_marker(board, player2_marker, position_input('Player 2'))
